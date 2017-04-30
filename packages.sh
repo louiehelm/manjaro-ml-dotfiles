0c8c4fdb5e4eca84020c2b712b99df01a32a4213
Louie Helm <lhelm@umich.edu>
2017-04-30 04:58:27 -0700

    better wal / gtk reloading

--- packages.sh
+++ packages.sh
@@ -22,21 +22,26 @@ sudo grep -q "editpkgbuild=false" /etc/xdg/pacaur/config || echo "editpkgbuild=f
 
 # Install desired packages
 sudo pacman -Rdd --noconfirm vte3
-sudo pacman -S --needed --noconfirm vte3-ng wget tmux rofi termite chromium mpv zsh numlockx qt5-styleplugins perl-anyevent-i3  hunspell-en lsof galculator gucharmap cmatrix doge httping
+sudo pacman -S --needed --noconfirm vte3-ng wget tmux rofi termite chromium mpv zsh numlockx qt5-styleplugins perl-anyevent-i3  hunspell-en lsof galculator gucharmap gnome-alsamixer cmatrix doge httping
 
 pacaur -S --needed --noedit --noconfirm polybar nerd-fonts-complete oh-my-zsh-git zsh-autosuggestions-git vim-lightline-git neofetch openblas-lapack sublime-text-dev
 
 # Update font cache
 fc-cache -fv
 
-# Make vim settings work even with sudo
+# Make vim / zsh settings work even with sudo
 sudo ln -sf /usr/bin/vim /usr/bin/vi
 sudo ln -sf ~/.vimrc /root/.vimrc
 sudo ln -sf ~/.vim /root/.vim
+
+sudo ln -sf ~/.zshrc /root/.zshrc
+sudo ln -sf ~/.config/script /root/.config/script
+sudo ln -sf ~/.cache/wal /root/.cache/wal
 
 
 # Switch to Zsh / Oh My Zsh + patch prompt to not break
 sudo sed -i 's/ prompt_status/#prompt_status/;s/\x27%~\x27/"${PWD\/#$HOME\/}"/;s/echo/print/;s/✚ //;s/✚//;s/●//;' /usr/share/oh-my-zsh/themes/agnoster.zsh-theme
 
 sudo chsh -s $(which zsh) $LOGNAME
+sudo chsh -s $(which zsh) root
 
